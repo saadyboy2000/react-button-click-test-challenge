120051d1ffb7c790f0326a5a12d20493fc3540af
saadyboy2000 <33137258+saadyboy2000@users.noreply.github.com>
2018-04-04 14:40:53 -0700

    Update button.test.js

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+const callback = jest.fn();
 
 import Button from './button';
 
@@ -7,5 +8,6 @@ describe('<Button />', () => {
     it('Should call props.onClick when the button is clicked', () => {
         const wrapper = shallow(<Button />);
         wrapper.find('button').simulate('click');
+        expect(callback).toHaveBeenCalledWith();
     });
 });
